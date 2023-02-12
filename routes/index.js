eb6152ceffd38829b52c21355451cc22ad310e24
Alberto Pérez <apleon89@gmail.com>
2023-02-12 19:41:10 +0000

    iteration 4 & 5 done

--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,36 @@ router.get('/artist-search', async (req, res, next) => {
     const artist = req.query.artist
     try {
         const response = await spotifyApi.searchArtists(artist)
-        // console.log(response.body.artists.items[0].images)
         res.render('artist-search.hbs', {
             allArtists : response.body.artists.items
+        })
+    }
+    catch (error) {
+        next(error)
+    }
+})
+
+router.get('/albums/:artistId', async (req, res, next) => {
+
+    try {
+        const response = await spotifyApi.getArtistAlbums(req.params.artistId)
+        const artistName = await spotifyApi.getArtist(req.params.artistId)
+        res.render('albums.hbs', {
+            allAlbums: response.body.items,
+            artistName: artistName.body.name
+        })
+    }
+    catch (error) {
+        next(error)
+    }
+})
+
+router.get('/tracks/:albumId', async (req, res, next) => {
+
+    try {
+        const response = await spotifyApi.getAlbumTracks(req.params.albumId)
+        res.render('tracks.hbs', {
+            allTracks : response.body.items
         })
     }
     catch (error) {
